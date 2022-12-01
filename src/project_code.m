c47cef189a1563f31550688229764ee38b4c496e
Tanner Koza <jtk0018@auburn.edu>
2022-12-01 09:58:03 -0600

    Complete report

--- src/project_code.m
+++ src/project_code.m
@@ -64,7 +64,7 @@ x_initial = [-0.5 ; 0 ; deg2rad(30) ; 0];
 % Set the Controller:
    % 1 = State-feedback linear model
    % 2 = .... so on
-Controller = 1;
+Controller = 3;
 
 if Controller == 1
     K = k;
@@ -94,61 +94,61 @@ end
 
 % Run Simulation
 %part 3-5
-% [t,~,x,u1] = sim('new_Sim_Simulink');
+[t,~,x,u1] = sim('new_Sim_Simulink');
 %part6
-[t,~,x,u1,x_hat] = sim('new_Sim_Simulink2');
+% [t,~,x,u1,x_hat] = sim('new_Sim_Simulink2');
 
 %   x is the state vector of [x; x_dot; phi; phi_dot],  4x1 vector
 
 x1 = x(:,1);% Cart x position plot
 phi = x(:,3);
 theta = rad2deg((phi+pi()))-180;
-delx=x-x_hat;
+% delx=x-x_hat;
 
 %part3-5
-% plot(t,x1)
-% title('X vs Time')
-% xlabel('Time (s)')
-% ylabel('X (m)')
-% figure
-% plot(t,theta)
-% title('Theta vs Time')
-% xlabel('Time(s)')
-% ylabel('Theta(deg)')
-% figure
-% plot(t,u1)
-% title('Input vs Time')
-% ylabel('Force (N)')
-% xlabel('Time (s)')
-
-%part6
 plot(t,x1)
-hold on
-plot(t,x_hat(:,1))
 title('X vs Time')
 xlabel('Time (s)')
 ylabel('X (m)')
-legend('x','x hat')
 figure
 plot(t,theta)
-hold on
-plot(t,rad2deg(x_hat(:,3)+pi())-180)
 title('Theta vs Time')
 xlabel('Time(s)')
 ylabel('Theta(deg)')
-legend('theta','theta hat')
 figure
 plot(t,u1)
 title('Input vs Time')
 ylabel('Force (N)')
 xlabel('Time (s)')
-figure
-plot(t,delx(:,1))
-ylabel('Error X')
-xlabel('Time (s)')
-title('Error Over Time X')
-figure
-plot(t,rad2deg(delx(:,3)))
-ylabel('Error Phi')
-xlabel('Time (s)')
-title('Error Over Time Phi')
+
+%part6
+% plot(t,x1)
+% hold on
+% plot(t,x_hat(:,1))
+% title('X vs Time')
+% xlabel('Time (s)')
+% ylabel('X (m)')
+% legend('x','x hat')
+% figure
+% plot(t,theta)
+% hold on
+% plot(t,rad2deg(x_hat(:,3)+pi())-180)
+% title('Theta vs Time')
+% xlabel('Time(s)')
+% ylabel('Theta(deg)')
+% legend('theta','theta hat')
+% figure
+% plot(t,u1)
+% title('Input vs Time')
+% ylabel('Force (N)')
+% xlabel('Time (s)')
+% figure
+% plot(t,delx(:,1))
+% ylabel('Error X')
+% xlabel('Time (s)')
+% title('Error Over Time X')
+% figure
+% plot(t,rad2deg(delx(:,3)))
+% ylabel('Error Phi')
+% xlabel('Time (s)')
+% title('Error Over Time Phi')
